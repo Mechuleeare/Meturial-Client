708160e9edc1197034235ecc1315fef916697b3e
Jin hyeon <kangjin627@gmail.com>
2023-10-30 12:33:41 +0900

    FIX: 라우팅 추가

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 import styled from 'styled-components/native';
 import Button from './components/Button';
+import Start from './components/Screens/Start';
 import Txt from './components/Txt';
 
 const HelloWorldApp = () => {
+
+  const Stack = createStackNavigator();
+
   return (
-    <Container>
-      <Text>Hello, world!</Text>
-      <Button title="조태곤" />
-      <Txt>조태곤</Txt>
-    </Container>
+    // <Container>
+    //   <Text>Hello, world!</Text>
+    //   <Button title="조태곤" />
+    //   <Txt>조태곤</Txt>
+    // </Container>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Start'>
+        <Stack.Screen name='Start' component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default HelloWorldApp;
