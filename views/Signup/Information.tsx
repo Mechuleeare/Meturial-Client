7e0be9fe372ef1c99a253f060cdf3e89f6643a77
강진현 <kangjin627@gmail.com>
2023-11-13 20:22:59 +0900

    ADD: Infromation Sucess

--- views/Signup/Information.tsx
+++ views/Signup/Information.tsx
@@ -5,8 +5,15 @@ import {BackArrow, SignupTitle} from '../../assets';
 import Txt from '../../components/Txt';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import {useState} from 'react';
 
 export const Signup = ({navigation}: any) => {
+  const [name, setName] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [number, setNumber] = useState<string>();
+  const [nextCheck, setNextCheck] = useState<boolean>(false);
+
   return (
     <Background>
       <BackPageFlex>
@@ -23,26 +30,44 @@ export const Signup = ({navigation}: any) => {
           title="이름"
           placeholder="이름을 입력해 주세요"
           eyeCheck={false}
+          fun={setName}
         />
         <Input
           title="비밀번호"
           placeholder="영문 숫자 특수기호 포함 8자리 이상"
           eyeCheck={true}
+          fun={setPassword}
         />
         <Input
           title="이메일"
           placeholder="이메일을 입력해 주세요"
           eyeCheck={false}
           email="email"
+          fun={setEmail}
+          emailValue={email}
         />
         <Input
           title="인증번호"
           placeholder="인증번호를 입력해 주세요"
           eyeCheck={false}
           email="number"
+          fun={setNumber}
+          emailValue={email}
+          numberValue={number}
+          check={setNextCheck}
         />
       </InputFlex>
-      <Button onPress={() => navigation.navigate('Profile')}>다음</Button>
+      <Button
+        onPress={() =>
+          nextCheck &&
+          navigation.navigate('Profile', {
+            email: email,
+            password: password,
+            name: name,
+          })
+        }>
+        다음
+      </Button>
       <SignupGo>
         <Txt typography="BodyMedium">이미 계정이 있으신가요?</Txt>
         <Pressable onPress={() => navigation.navigate('Login')}>
@@ -77,7 +102,7 @@ const TitleFlex = styled.View`
 const InputFlex = styled.View`
   display: flex;
   width: 100%;
-  height: 63%;
+  height: 70%;
   gap: 18px;
 `;
 
