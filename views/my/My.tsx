4bee669847994a6d45ae44072cf1fc1a651b32c2
강진현 <kangjin627@gmail.com>
2023-11-15 23:45:20 +0900

    ADD: MyReview ADD

--- views/my/My.tsx
+++ views/my/My.tsx
@@ -5,8 +5,44 @@ import {Image} from 'react-native';
 import {Gear, Pencil} from '../../assets';
 import Button from '../../components/Button';
 import ReviewPreview from '../../components/ReviewPreview';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {BaseUrl} from '../../utils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const My = () => {
+interface RecipeType {
+  content: string;
+  createdAt: string;
+  recipeName: string;
+  reviewId: string;
+  reviewImageUrl: string;
+  starRating: number;
+}
+
+export const My = ({navigation}: any) => {
+  const [recipe, setRecipe] = useState<RecipeType[]>([]);
+
+  useEffect(() => {
+    async function AxiosMyApi() {
+      const Token = await AsyncStorage.getItem('AccessToken');
+      try {
+        const result = await axios.get(`${BaseUrl}/review/my`, {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+        });
+        console.log(result.data);
+        const recipeData = result.data.myReviewList;
+        const sliceData = recipeData.slice(0, 3);
+        setRecipe(sliceData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    AxiosMyApi();
+  }, []);
+
   return (
     <Background>
       <Header>
@@ -14,41 +50,61 @@ export const My = () => {
         <Image source={Gear} />
       </Header>
       <Line />
-      <Information>
-        <Profile
-          source={{
-            uri: 'https://s3.ap-northeast-2.amazonaws.com/meturial-bucket/08efe72f-e897-4a63-b202-1cc012d85a91test%201.jpg',
-          }}
-        />
-        <Margin>
-          <Txt typography="TitleLarge">최승우</Txt>
-          <Txt typography="BodyMedium" color={color.Gray[600]}>
-            wjknn3123@gmail.com
-          </Txt>
-        </Margin>
-        <Button status="outline" icon={<Pencil size={20} />}>
-          내정보 수정하기
-        </Button>
-      </Information>
-      <AllergyInfromation>
-        <Txt typography="TitleMedium">알레르기</Txt>
-        <AllergyFlex>
-          <Allergy>
-            <Txt typography="LabelSmall">복숭아</Txt>
-          </Allergy>
-        </AllergyFlex>
-      </AllergyInfromation>
-      <MyReview>
-        <ReviewTitle>
-          <Txt typography="TitleMedium">내가 작성한 후기</Txt>
-          <Txt typography="TitleMedium" color={color.Green.Point}>
-            12
-          </Txt>
-        </ReviewTitle>
-        <ReviewFlex>
-          <ReviewPreview></ReviewPreview>
-        </ReviewFlex>
-      </MyReview>
+      <ScrollView>
+        <Information>
+          <Profile
+            source={{
+              uri: 'https://s3.ap-northeast-2.amazonaws.com/meturial-bucket/08efe72f-e897-4a63-b202-1cc012d85a91test%201.jpg',
+            }}
+          />
+          <Margin>
+            <Txt typography="TitleLarge">최승우</Txt>
+            <Txt typography="BodyMedium" color={color.Gray[600]}>
+              wjknn3123@gmail.com
+            </Txt>
+          </Margin>
+          <Button status="outline" icon={<Pencil size={20} />}>
+            내정보 수정하기
+          </Button>
+        </Information>
+        <AllergyInfromation>
+          <Txt typography="TitleMedium">알레르기</Txt>
+          <AllergyFlex>
+            <Allergy>
+              <Txt typography="LabelSmall">복숭아</Txt>
+            </Allergy>
+          </AllergyFlex>
+        </AllergyInfromation>
+        <MyReview>
+          <ReviewTitle>
+            <Txt typography="TitleMedium">내가 작성한 후기</Txt>
+            <Txt typography="TitleMedium" color={color.Green.Point}>
+              12
+            </Txt>
+          </ReviewTitle>
+          <ReviewFlex>
+            {recipe.map(v => (
+              <ReviewPreview
+                key={v.reviewId}
+                name={v.recipeName}
+                starRating={v.starRating}
+                content={v.content}
+                reviewImageUrl={v.reviewImageUrl}
+                createdAt={v.createdAt}
+                onTouch={() =>
+                  navigation.navigate('Review', {
+                    recipe: v.recipeName,
+                    data: v.reviewId,
+                  })
+                }
+              />
+            ))}
+          </ReviewFlex>
+          <Button status="outline" onPress={navigation.navigate('A')}>
+            내가 작성한 후기 모두 보기
+          </Button>
+        </MyReview>
+      </ScrollView>
     </Background>
   );
 };
@@ -73,6 +129,11 @@ const Line = styled.View`
   width: 100%;
   height: 1px;
   background-color: ${color.Gray[100]};
+`;
+
+const ScrollView = styled.ScrollView`
+  width: 100%;
+  flex: 1;
 `;
 
 const Information = styled.View`
@@ -112,6 +173,8 @@ const Allergy = styled.View`
 `;
 
 const MyReview = styled.View`
+  width: 100%;
+  flex: 1;
   padding: 26px 16px;
   background-color: ${color.White};
   margin-top: 12px;
@@ -124,5 +187,6 @@ const ReviewTitle = styled.View`
 
 const ReviewFlex = styled.View`
   margin-top: 24px;
+  margin-bottom: 24px;
   gap: 18px;
 `;
