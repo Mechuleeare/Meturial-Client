1ccc36cb39a5c8cf5c124b5c53cbdc9f5a22ca25
SeungWoo <wjknn3123@gmail.com>
2023-11-16 19:33:31 +0900

    ADD : getCategoryRecipe api

--- views/recipe/CategoryRecipe.tsx
+++ views/recipe/CategoryRecipe.tsx
@@ -1,9 +1,26 @@
 import styled from 'styled-components/native';
 import {color} from '../../style/color';
 import BackHeader from '../../components/BackHeader';
+import {useEffect} from 'react';
+import axios from 'axios';
+import {Access_Token} from '../main/Main';
 
 const CategoryRecipe = ({route, navigation}: any) => {
   const {recipe} = route.params;
+
+  useEffect(() => {
+    async function getCategoryRecipe() {
+      await axios
+        .get('http://43.202.18.230:8000/recipe/category', {
+          headers: {Authorization: Access_Token},
+          params: {category: '한식'},
+        })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+    }
+
+    getCategoryRecipe();
+  }, []);
 
   return (
     <Frame>
