d536caaa1d19356cc52a245b7c3b30e0fb1fc61f
SeungWoo <wjknn3123@gmail.com>
2023-11-16 22:21:50 +0900

    EDIT : Recipe Top Bar done

--- views/recipe/Recipe.tsx
+++ views/recipe/Recipe.tsx
@@ -1,5 +1,16 @@
 import {Text} from 'react-native';
+import styled from 'styled-components/native';
+import {color} from '../../style/color';
 
 export const Recipe = () => {
-  return <Text>this is recipe page</Text>;
+  return (
+    <Frame>
+      <Text>this is recipe page</Text>
+    </Frame>
+  );
 };
+
+const Frame = styled.View`
+  flex: 1;
+  background-color: ${color.White};
+`;
