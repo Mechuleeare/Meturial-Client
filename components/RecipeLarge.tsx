c158b7f919233fa18d7bfa8fa279a11a8bba3d8b
SeungWoo <wjknn3123@gmail.com>
2023-11-16 19:26:08 +0900

    ADD : getCategory & Skeleton

--- components/RecipeLarge.tsx
+++ components/RecipeLarge.tsx
@@ -4,44 +4,80 @@ import Txt from './Txt';
 import {Star_filled} from '../assets';
 import WishButton from './WishButton';
 import {recommendDataRes} from '../views';
+import {View} from 'react-native';
 
-const RecipeLarge = ({nav, data}: {nav: any; data: recommendDataRes}) => {
-  const {name, bigtype, material, description, url} = data;
-  let kor = url.split('/');
-  let encodeTxt = encodeURIComponent(kor[4]);
-  kor[4] = encodeTxt;
-  let result = kor.join('/');
-  console.log(result);
+const RecipeLarge = ({
+  nav,
+  data,
+}: {
+  nav?: any;
+  data?: recommendDataRes | undefined;
+}) => {
+  function urlEncoding(url: string) {
+    let kor = url.split('/');
+    let encodeTxt = encodeURIComponent(kor[4]);
+    kor[4] = encodeTxt;
+    let result = kor.join('/');
+    console.log(result);
+    return result;
+  }
   return (
-    <RecipeFrame
-      onTouchEnd={() => nav.navigate('DetailRecipe', {recipe: name})}>
-      <BigImg source={{uri: `${result}`}} />
-      <Title>
-        <Txt typography="TitleMedium">{name}</Txt>
-        <WishButton size={18} />
-      </Title>
-      <TagFrame>
-        <Txt typography="BodySmall" color={color.Green[500]}>
-          #{bigtype}
-        </Txt>
-      </TagFrame>
-      <SubFrame>
-        <Txt typography="LabelSmall" color={color.Gray[300]}>
-          2048명의 후기
-        </Txt>
-        <StarFrame>
-          <Star_filled size={16} color={color.Yellow.Point} />
-          <Txt typography="BodySmall" color={color.Yellow[900]}>
-            4.8
-          </Txt>
-        </StarFrame>
-      </SubFrame>
-    </RecipeFrame>
+    <View>
+      {data ? (
+        <RecipeFrame
+          onTouchEnd={() => nav.navigate('DetailRecipe', {recipe: data.name})}>
+          <BigImg source={{uri: `${urlEncoding(data.url)}`}} />
+          <Title>
+            <Txt typography="TitleMedium">{data.name}</Txt>
+            <WishButton size={18} />
+          </Title>
+          <TagFrame>
+            <Txt typography="BodySmall" color={color.Green[500]}>
+              #{data.bigtype}
+            </Txt>
+          </TagFrame>
+          <SubFrame>
+            <Txt typography="LabelSmall" color={color.Gray[300]}>
+              2048명의 후기
+            </Txt>
+            <StarFrame>
+              <Star_filled size={16} color={color.Yellow.Point} />
+              <Txt typography="BodySmall" color={color.Yellow[900]}>
+                4.8
+              </Txt>
+            </StarFrame>
+          </SubFrame>
+        </RecipeFrame>
+      ) : (
+        <RecipeFrame>
+          <Skeleton height={180} width={180} />
+          <Title>
+            <Skeleton height={24} width={80} />
+            <Skeleton height={18} width={18} />
+          </Title>
+          <TagFrame>
+            <Skeleton width={40} />
+          </TagFrame>
+          <SubFrame>
+            <Skeleton width={100} />
+            <StarFrame>
+              <Skeleton width={36} />
+            </StarFrame>
+          </SubFrame>
+        </RecipeFrame>
+      )}
+    </View>
   );
 };
 
 export default RecipeLarge;
 
+const Skeleton = styled.View<{height?: number; width: number}>`
+  background-color: ${color.Gray[50]};
+  border-radius: 8px;
+  height: ${props => props.height || 16}px;
+  width: ${props => props.width}px;
+`;
 const StarFrame = styled.View`
   flex-direction: row;
   align-items: center;
--- views/main/Main.tsx
+++ views/main/Main.tsx
@@ -36,19 +36,8 @@ const todayMenu: todayMenuProps = {
   },
 };
 
-const category: string[] = [
-  '한식',
-  '양식',
-  '일식',
-  '중식',
-  '분식',
-  '음료',
-  '디저트',
-  '면요리',
-];
-
 const Access_Token =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3amtubjMxMjNAZ21haWwuY29tIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTcwMDA1MTA2MiwiZXhwIjoxNzAwMDU4MjYyfQ.w6j9Nh0leIY5MPIWQ8Y62JBJ_ZFqJAN_ba9-wZ3Esx7l-HFGEpo4gH-Yo-aVkLqDv4XqLAmNmlXfThvEk4XllA';
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3amtubjMxMjNAZ21haWwuY29tIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTcwMDEyNzkxMSwiZXhwIjoxNzAwMTM1MTExfQ.W7a-omzbinyKQfj1iFs3q8-sLm1iGs4F-Syisaog6-rku_c8rC00uMZcfJDID_GnbzP_SZ4hsm6EvaAXphK-VQ';
 
 export interface recommendDataRes {
   name: string;
@@ -58,16 +47,32 @@ export interface recommendDataRes {
   url: string;
 }
 
+interface categoryRes {
+  categoryName: string;
+  categoryImageUrl: string;
+}
+
 export const Main = ({navigation}: any) => {
-  const [recommendData, setRecommendData] = useState<recommendDataRes[]>([]);
+  const [recommendData, setRecommendData] = useState<
+    recommendDataRes[] | undefined
+  >(undefined);
+  const [category, setCategory] = useState<categoryRes[] | undefined>(
+    undefined,
+  );
 
   useEffect(() => {
     async function getRecommendRecipe() {
       await axios
-        .get('http://192.168.1.113:8000/recipe/today', {
+        .get('http://43.202.18.230:8000/recipe/today', {
           headers: {Autorization: `Bearer ${Access_Token}`},
         })
-        .then(res => setRecommendData(res.data))
+        .then(res => setRecommendData([res.data]))
+        .catch(err => console.log(err));
+      await axios
+        .get('http://43.202.18.230:80/recipe/category', {
+          headers: {Authorization: `Bearer ${Access_Token}`},
+        })
+        .then(res => setCategory(res.data.category))
         .catch(err => console.log(err));
     }
 
@@ -105,16 +110,30 @@ export const Main = ({navigation}: any) => {
         <Box>
           <UnderTxt>레시피 카테고리</UnderTxt>
           <CategoryFrame>
-            {category.map(v => (
-              <ItemBox
-                key={v}
-                onTouchEnd={() =>
-                  navigation.navigate('CategoryRecipe', {recipe: v})
-                }>
-                <ItemImg />
-                <Txt typography="LabelMedium">{v}</Txt>
-              </ItemBox>
-            ))}
+            {category
+              ? category.map(v => (
+                  <ItemBox
+                    key={v.categoryName}
+                    onTouchEnd={() =>
+                      navigation.navigate('CategoryRecipe', {recipe: v})
+                    }>
+                    <ItemImg />
+                    <Txt typography="LabelMedium">{v.categoryName}</Txt>
+                  </ItemBox>
+                ))
+              : [1, 2, 3, 4, 5, 6, 7, 8].map(v => (
+                  <ItemBox key={v}>
+                    <ItemImg />
+                    <View
+                      style={{
+                        backgroundColor: color.Gray[50],
+                        height: 18,
+                        width: 32,
+                        borderRadius: 8,
+                      }}
+                    />
+                  </ItemBox>
+                ))}
           </CategoryFrame>
         </Box>
         <Box>
@@ -132,9 +151,11 @@ export const Main = ({navigation}: any) => {
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{paddingHorizontal: 16, gap: 8}}>
-          {recommendData.slice(0, 6).map((v, i) => (
-            <RecipeLarge nav={navigation} data={v} key={i} />
-          ))}
+          {recommendData
+            ? recommendData.map((v, i) => (
+                <RecipeLarge nav={navigation} data={v} key={i} />
+              ))
+            : [1, 2, 3, 4, 5, 6].map(v => <RecipeLarge key={v} />)}
         </RecommendRecipe>
       </Content>
     </Frame>
