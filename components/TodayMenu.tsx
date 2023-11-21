0de182f81cc2e77866ea2da59e4a5c51da21f275
강진현 <kangjin627@gmail.com>
2023-11-21 20:20:42 +0900

    ADD: 원래 코드로 되돌림

--- components/TodayMenu.tsx
+++ components/TodayMenu.tsx
@@ -3,58 +3,51 @@ import styled from 'styled-components/native';
 import {color} from '../style/color';
 import Txt from './Txt';
 import {Arrow_right} from '../assets';
-import {useEffect, useState} from 'react';
+import {menuType} from '../views';
 
-interface TodayMenuType {
-  id?: string;
-  img?: string;
-  time?: string;
-  nav?: any;
-  name?: string;
-}
+const todayTime = {
+  morning: ['아침', '8:00'],
+  lunch: ['점심', '12:00'],
+  dinner: ['저녁', '6:00'],
+};
 
-const TodayMenu = ({time, nav, img, id, name}: TodayMenuType) => {
-  const [timeData, setTimeData] = useState<string>();
-  const [timeName, setTimeName] = useState<string>();
-
-  useEffect(() => {
-    if (time === 'BREAKFAST') {
-      setTimeName('아침');
-      setTimeData('8:00');
-    } else if (time === 'LUNCH') {
-      setTimeName('점심');
-      setTimeData('12:00');
-    } else if (time === 'DINNER') {
-      setTimeName('저녁');
-      setTimeData('6:00');
-    }
-  }, [time]);
-
+const TodayMenu = ({
+  data,
+  time,
+  nav,
+}: {
+  data: menuType;
+  time: 'morning' | 'lunch' | 'dinner';
+  nav: any;
+}) => {
+  const {menu, img, recipe} = data;
   return (
     <View>
-      <TodayFrame>
-        <TextFrame>
-          <Txt typography="TitleSmall">{timeName}</Txt>
-          <Txt typography="HeadlineLarge">{timeData}</Txt>
-          <Txt typography="TitleMedium" style={{marginTop: 8}}>
-            {name}
-          </Txt>
-          <MoveFrame>
-            <Txt
-              typography="BodySmall"
-              color={color.Green[500]}
-              onPress={() => nav.navigate('DetailRecipe', {recipe: id})}>
-              레시피 보러가기
+      {data && (
+        <TodayFrame>
+          <TextFrame>
+            <Txt typography="TitleSmall">{todayTime[time][0]}</Txt>
+            <Txt typography="HeadlineLarge">{todayTime[time][1]}</Txt>
+            <Txt typography="TitleMedium" style={{marginTop: 8}}>
+              {menu}
             </Txt>
-            <Arrow_right color={color.Green[500]} />
-          </MoveFrame>
-        </TextFrame>
-        <ImgFrame
-          source={{
-            uri: img,
-          }}
-        />
-      </TodayFrame>
+            <MoveFrame>
+              <Txt
+                typography="BodySmall"
+                color={color.Green[500]}
+                onPress={() => nav.navigate('DetailRecipe', {recipe: recipe})}>
+                레시피 보러가기
+              </Txt>
+              <Arrow_right color={color.Green[500]} />
+            </MoveFrame>
+          </TextFrame>
+          <ImgFrame
+            source={{
+              uri: img,
+            }}
+          />
+        </TodayFrame>
+      )}
     </View>
   );
 };
