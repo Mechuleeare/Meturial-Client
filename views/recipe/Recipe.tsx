43349d532569d841d7a306d07d0d97f1ca09cbe1
SeungWoo <wjknn3123@gmail.com>
2023-11-29 19:36:12 +0900

    EDIT : Recipe page input move to RecipeTopBar

--- views/recipe/Recipe.tsx
+++ views/recipe/Recipe.tsx
@@ -1,31 +1,11 @@
 import styled from 'styled-components/native';
 import {color} from '../../style/color';
-import {Search} from '../../assets';
 import Txt from '../../components/Txt';
 import {CategoriData} from '../../utils';
 
 export const Recipe = ({navigation}: any) => {
   return (
-    <Frame>
-      <Gap>
-        <InputFrame
-          style={{
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 0,
-            },
-            shadowOpacity: 0.12,
-            shadowRadius: 16.0,
-            elevation: 24,
-          }}>
-          <Input
-            placeholder="레시피를 검색해보세요."
-            placeholderTextColor={color.Gray[400]}
-          />
-          <Search color={color.Gray[400]} />
-        </InputFrame>
-      </Gap>
+    <Frame contentContainerStyle={{paddingTop: 24}}>
       <CategoryFrame>
         {CategoriData.map(v => (
           <ItemBox
@@ -60,24 +40,6 @@ const CategoryFrame = styled.View`
   row-gap: 12px;
   column-gap: 36px;
   padding: 0 16px;
-`;
-const Input = styled.TextInput`
-  font-size: 16px;
-  line-height: 24px;
-  flex: 1;
-`;
-const InputFrame = styled.View`
-  border-radius: 8px;
-  background-color: ${color.White};
-  padding: 0 8px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  gap: 8px;
-`;
-const Gap = styled.View`
-  width: 100%;
-  padding: 24px 16px;
 `;
 const Frame = styled.ScrollView`
   flex: 1;
